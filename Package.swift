1e496b8dc03832efd18e3042940a22cb996274e2
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-07-02 10:54:54 +0200

    update supported platform and Swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,11 @@ import PackageDescription
 let package = Package(
     name: "opentelemetry-swift",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v12),
+        .visionOS(.v1),
+        .watchOS(.v7)
     ],
     products: [
         .library(name: "OpenTelemetryApi", targets: ["OpenTelemetryApi"]),
